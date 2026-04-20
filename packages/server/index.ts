9c67d72208c9b19336aaaa17decf75dc27373f1f
IuliaV <iuliamvoicu@yahoo.com>
2026-04-20 13:38:31 +0300

    Managwe openai api key

---
+++ packages/server/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import type { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req: Request, res: Response) => res.send("Hello World, meow!"));
+app.listen(port, () =>
+  console.log(`Server up and running on port http://localhost:${port}`),
+);
