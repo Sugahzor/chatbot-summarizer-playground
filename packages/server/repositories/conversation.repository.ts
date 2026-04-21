30e5f5870d426b937f4dd72a57fd576120819a97
IuliaV <iuliamvoicu@yahoo.com>
2026-04-21 10:12:50 +0300

    implement sendMessage api and functionality

---
+++ packages/server/repositories/conversation.repository.ts
@@ -0,0 +1,14 @@
+const conversations = new Map<string, string>();
+
+export const conversationRepository = {
+  getLastResponseId,
+  setLastResponseId,
+};
+
+function getLastResponseId(conversationId: string): string | undefined {
+  return conversations.get(conversationId);
+}
+
+function setLastResponseId(conversationId: string, responseId: string): void {
+  conversations.set(conversationId, responseId);
+}
